72866c034ab2d202d2d68dbe23dbd0e7ebbfeed5
Zhejiong Chen <zhejiong.24@intl.zju.edu.cn>
2025-03-12 15:02:07 +0800

    change psuedocode2

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,10 +1,12 @@
 weight = 67  #input weight
 height = 1.81  #input height
-BMI = weight/(height)**2  #calculate BMI
+BMI = weight/(height)**2  #calculate BMI according to the equation
 
-if BMI > 30:  #use if-elif-else to adapt to three circumstances
-    print("your BMI is", str(BMI), ", and you are categorized as obese.")
+##use if-elif-else to adapt to three circumstance
+
+if BMI > 30:  
+    print("your BMI is", str(BMI), ", and you are categorized as obese.")#if BMI>30, obese
 elif BMI < 18.5:
-    print("your BMI is", str(BMI), ", and you are categorized as underweight.")
+    print("your BMI is", str(BMI), ", and you are categorized as underweight.")#if BMI<18.5, underweight
 else:
-    print("your BMI is", str(BMI), ", and you are categorized as normal.")
+    print("your BMI is", str(BMI), ", and you are categorized as normal.")#other circumstances, normal
