4a443509e3cca0cf62c6a613492e28a516e2a7a6
Zhejiong Chen <zhejiong.24@intl.zju.edu.cn>
2025-04-23 20:46:42 +0800

    change pseudocode

--- Practical5/1.py
+++ Practical5/1.py
@@ -1,20 +1,24 @@
-#input the data
+#input the data as a list
+#import matplot and NumPy
+#save the names of languages to the variable "languages"
+#save the percentage of users to the variable "counts"
+#draw the bar chart plt.bar(ind, counts, width, yerr=Std)
 data = {'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5}
 
 #draw the bar chart
-import matplotlib.pyplot as plt   #import matplot and NumPy
+import matplotlib.pyplot as plt 
 import numpy as np
 N = 5  #the number of languages
-languages = list(data.keys())  #the names of languages
-counts = list(data.values())  #the percentage of users
+languages = list(data.keys())
+counts = list(data.values())
 Std = (0, 0, 0, 0, 0)  #the range of error of each language equals to 0 
 ind = np.arange(N)  #Generate position array [0, 1, 2, 3, 4]
 width = 0.35  #Width of each bar
 p1 = plt.bar(ind, counts, width, yerr=Std)
 plt.ylabel('percentage')  #Set y-axis label
-plt.title('the percentage of developers who use the following languages')  #Set title
-plt.xticks(ind, ('JavaScript', 'HTML', 'Python', 'SQL','TypeScript'))  #Label x-axis as language names
-plt.yticks(np.arange(0, 110, 10))  #Set y-axis range
+plt.title('the percentage of developers who use the following languages')
+plt.xticks(ind, ('JavaScript', 'HTML', 'Python', 'SQL','TypeScript')) 
+plt.yticks(np.arange(0, 110, 10))
 plt.show()
 
 #print the percentage of JavaScript users
--- Practical5/2.py
+++ Practical5/2.py
@@ -1,12 +1,13 @@
 #input the data list
+#sort the data using sorted() function
+#draw the two pie charts plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90)
+
 uk_countries=[57.11,3.13,1.91,5.45] 
 zjneighbouring_provinces=[65.77,41.88,45.28,61.27,85.15]
 
-#sort the data
 print(sorted(uk_countries))
 print(sorted(zjneighbouring_provinces))
 
-#draw the two pie charts
 import matplotlib.pyplot as plt
 labels = 'England', 'Wales', 'Northern Ireland', 'Scotland'
 sizes = [57.11,3.13,1.91,5.45]
